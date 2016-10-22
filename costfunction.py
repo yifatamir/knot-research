775aff557384f893d3da7355330e00cab6beafe5
Yifat Amir <yifatamir@berkeley.edu>
2016-10-21 17:08:16 -0700

    gradient descent first draft working

--- costfunction.py
+++ costfunction.py
@@ -4,10 +4,7 @@ def cost_function(nonahedron, alpha, beta, gamma):
 	eld = ELD(nonahedron)
 	pad = PAD(nonahedron)
 	fnp = FNP(nonahedron)
-	print("eld: " +  str(eld))
-	print("pad: " + str(pad))
-	print("fnp: " + str(fnp))
-	return alpha*eld + beta*pad + gamma*fnp
+	return (alpha*eld + beta*pad + gamma*fnp, eld, pad, fnp)
 
 def ELD(nonahedron):
 	all_edges = nonahedron.edges
@@ -24,19 +21,25 @@ def PAD(nonahedron):
 	quad_angles = []
 	for list_quad_angles in listlist_quad_angles:
 		quad_angles += list_quad_angles
-	quads_dev = sum([(angle - 90)**2 for angle in quad_angles])
+	quads_dev = sum([(angle - 0.5*np.pi)**2 for angle in quad_angles])
 	pentas = nonahedron.topPentas + nonahedron.bottomPentas
 	listlist_pent_edges = [nonahedron.getEdges(pent) for pent in pentas]
 	listlist_pent_angles = [angles_of_edges(list_pent_edges) for list_pent_edges in listlist_pent_edges]
 	pent_angles = []
 	for list_pent_angles in listlist_pent_angles:
 		pent_angles += list_pent_angles
-	pents_dev = sum([(angle - 108)**2 for angle in pent_angles])
+	pents_dev = sum([(angle - 0.6*np.pi)**2 for angle in pent_angles])
 	result = quads_dev + pents_dev
 	return result
 
 def FNP(nonahedron):
-	return 0
+	shapes = nonahedron.quads + nonahedron.topPentas + nonahedron.bottomPentas
+	result = 0
+	for shape in shapes:
+		plane = compute_plane(shape)
+		for point in shape:
+			result += dist_of_point_from_plane(point, plane)**2
+	return result
 
 #returns the Euclidean distance between two points that make up an edge. 
 #the edge passed in to this function is of type ((x1, y1, z1), (x2, y2, z2)).
@@ -59,11 +62,11 @@ def vectorize(edge):
 def unit_vector(vector):
     return vector / np.linalg.norm(vector)
 
-#returns the angle in degrees between two vectors. ex: angle_between((1, 0, 0), (-1, 0, 0)) will output 180
+#returns the angle in radians between two vectors. ex: angle_between((1, 0, 0), (-1, 0, 0)) will output pi/2
 def angle_between(v1, v2):
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
-    return np.rad2deg(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 #takes in the list of edges in a shape [(p1, p2), (p2, p3), (p3, p1)] and returns a list of angles in that shape.
 # ex: [60, 60, 60]
@@ -78,7 +81,98 @@ def angles_of_edges(list_of_edges):
 		vector_of_edge = vectorize(edge)
 		vector_of_next_edge = vectorize(next_edge)
 		angle = abs(angle_between(vector_of_edge, vector_of_next_edge))
-		if (angle > 180):
-			angle = 360 - angle
+		if (angle > np.pi):
+			angle = 2*np.pi - angle
 		angles += [angle]
 	return angles
+
+def x(point):
+	return point[0]
+
+def y(point):
+	return point[1]
+
+def z(point):
+	return point[2]
+
+#returns the value of A in http://cs.haifa.ac.il/~gordon/plane.pdf using the given shape (p1, p2, ... pn)
+def computeA(shape):
+	a_sum = 0
+	num_points = len(shape)
+	for i in range(0, num_points):
+		point_i = shape[i]
+		if (i == num_points - 1):
+			point_ip1 = shape[0]
+		else:
+			point_ip1 = shape[i + 1]
+		a_sum += (y(point_i) - y(point_ip1)) * (z(point_i) + z(point_ip1))
+	return a_sum
+
+#returns the value of B in http://cs.haifa.ac.il/~gordon/plane.pdf using the given shape (p1, p2, ... pn)
+def computeB(shape):
+	b_sum = 0
+	num_points = len(shape)
+	for i in range(0, num_points):
+		point_i = shape[i]
+		if (i == num_points - 1):
+			point_ip1 = shape[0]
+		else:
+			point_ip1 = shape[i + 1]
+		b_sum += (z(point_i) - z(point_ip1)) * (x(point_i) + x(point_ip1))
+	return b_sum
+
+#returns the value of B in http://cs.haifa.ac.il/~gordon/plane.pdf using the given shape (p1, p2, ... pn)
+def computeC(shape):
+	c_sum = 0
+	num_points = len(shape)
+	for i in range(0, num_points):
+		point_i = shape[i]
+		if (i == num_points - 1):
+			point_ip1 = shape[0]
+		else:
+			point_ip1 = shape[i + 1]
+		c_sum += (x(point_i) - x(point_ip1)) * (y(point_i) + y(point_ip1))
+	return c_sum
+
+#returns the value of P (centroid point; center of gravity) in 
+#http://cs.haifa.ac.il/~gordon/plane.pdf using the given shape (p1, p2, ... pn)
+def computeP(shape):
+	p_sum = (0, 0, 0)
+	num_points = len(shape)
+	for i in range(0, num_points):
+		point_i = shape[i]
+		p_sum = (x(p_sum) + x(point_i), y(p_sum) + y(point_i), z(p_sum) + z(point_i))
+	p_sum = (x(p_sum)/num_points, y(p_sum)/num_points, z(p_sum)/num_points)
+	return p_sum
+
+#returns the value of B in http://cs.haifa.ac.il/~gordon/plane.pdf using the given shape (p1, p2, ... pn)
+def computeD(shape, a, b, c):
+	p = computeP(shape)
+	n = (a, b, c)
+	return -1 * dot_product(p, n)
+
+#takes in two vectors of the form (a, b, ... z) and returns their scalar dot product.
+def dot_product(v1, v2):
+	result = 0
+	if len(v1) != len(v2):
+		return -1
+	for i in range(0, len(v1)):
+		result += v1[i] * v2[i]
+	return result
+
+#computes the distance from point (x, y, z) to plane (a, b, c, d).
+def dist_of_point_from_plane(point, plane):
+	x, y, z = point
+	a, b, c, d = plane
+	return a*x + b*y + c*z + d
+	
+#returns the value of (A, B, C, D) for the best fitting plane of the given shape.
+def compute_plane(shape):
+	a = computeA(shape)
+	b = computeB(shape)
+	c = computeC(shape)
+	d = computeD(shape, a, b, c)
+	return (a, b, c, d)
+
+
+
--- nearmissjohnson.py
+++ nearmissjohnson.py
@@ -10,7 +10,6 @@ import costfunction as c
 
 eye = np.array([0.0,-13.0,1.5])
 up = np.array([0.0,0.0,1.0])
-
 prevX = 0
 prevY = 0
 r = 0
@@ -25,21 +24,93 @@ radius_step = 0.01
 height_step = 0.01
 z_step = 0.01
 
-alpha = 1000000
-beta = 0.0001
+global_step = 0.01
+
+alpha = 1
+beta = 1
 gamma = 1
 
+end = False
+
 non = nona.Nonahedron(x_edge, radius_corner, height_corner, top_z)
-cost = c.cost_function(non, alpha, beta, gamma)
+cost, eld, pad, fnp = c.cost_function(non, alpha, beta, gamma)
+lowest_cost = cost
 past_cost = 0
 
+colors = (
+	(0,0,1),
+	(0,1,0),
+	(1,0,0),
+	(1,1,0),
+	(1,0,1),
+	(0,1,1),
+	(0,0,0),
+	(1,1,1),
+	)
+
+def gradient_descent():
+	global non, cost, global_step
+	base_cost = cost
+	non.ex += global_step
+	non.rebuild()
+	cost, eld, pad, fnp = c.cost_function(non, alpha, beta, gamma)
+	delta_x = cost - base_cost
+	non.ex -= global_step
+	non.rc += global_step
+	non.rebuild()
+	cost, eld, pad, fnp = c.cost_function(non, alpha, beta, gamma)
+	delta_r = cost - base_cost
+	non.rc -= global_step
+	non.hc += global_step
+	non.rebuild()
+	cost, eld, pad, fnp = c.cost_function(non, alpha, beta, gamma)
+	delta_h = cost - base_cost
+	non.hc -= global_step
+	non.tz += global_step
+	non.rebuild()
+	cost, eld, pad, fnp = c.cost_function(non, alpha, beta, gamma)
+	delta_t = cost - base_cost
+	non.tz -= global_step
+	highest_sensitivity = max([abs(x) for x in [delta_t, delta_h, delta_r, delta_x]])
+	if highest_sensitivity == abs(delta_x):
+		if (delta_x < 0):
+			non.ex += global_step
+		else:
+			non.ex -= global_step
+	elif highest_sensitivity == abs(delta_r):
+		if (delta_r < 0):
+			non.rc += global_step
+		else:
+			non.rc -= global_step
+	elif highest_sensitivity == abs(delta_h):
+		if (delta_h < 0):
+			non.hc += global_step
+		else:
+			non.hc -= global_step
+	else:
+		if (delta_t < 0):
+			non.tz += global_step
+		else:
+			non.tz -= global_step
+	non.rebuild()
+	cost, eld, pad, fnp = c.cost_function(non, alpha, beta, gamma)
+	if (abs(cost - base_cost) < 0.0001 || end):
+		if (cost - lowest_cost > 0):
+			print("STOP GRADIENT DESCENT!!!!")
+			global_step = float(global_step) / 2
+			end = True
+		else:
+			global_step = global_step * 2
+	if (abs(cost - base_cost) > 0.1):
+		global_step = float(global_step) / 2
+
 def init():
-	glClearColor(0.0, 0.0, 0.0, 1.0); # Set background color to black and opaque
-	glClearDepth(1.0);                   # Set background depth to farthest
-	glEnable(GL_DEPTH_TEST);   # Enable depth testing for z-culling
-	glDepthFunc(GL_LEQUAL);    # Set the type of depth-test
-	glShadeModel(GL_SMOOTH);   # Enable smooth shading
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glClearColor(0.0, 0.0, 0.0, 1.0) # Set background color to black and opaque
+	glClearDepth(1.0)                   # Set background depth to farthest
+	glEnable(GL_DEPTH_TEST)   # Enable depth testing for z-culling
+	glDepthFunc(GL_LEQUAL)    # Set the type of depth-test
+	glShadeModel(GL_SMOOTH)   # Enable smooth shading
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
 
 def mainDisplay():
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -65,16 +136,32 @@ def mainDisplay():
 	glEnd()
 
 	#Draw Geometry
-	global non, cost, past_cost
+	global non, cost, past_cost, lowest_cost, colors, eld, pad, fnp
 	non.rebuild()
-	cost = c.cost_function(non, alpha, beta, gamma)
+	cost, eld, pad, fnp = c.cost_function(non, alpha, beta, gamma)
 	if (cost != past_cost):
-		print(cost)
+		print("eld: " +  str(eld) + "     pad: " + str(pad) + "     fnp: " + str(fnp))
+		print(cost - lowest_cost)
+		lowest_cost = min(lowest_cost, cost)
 		past_cost = cost
 
+	# surfaces = non.topPentas + non.bottomPentas + non.quads
+	surfaces = non.quads
+	x = 0
+	a = 0
+	for surface in surfaces:
+		if a == 3:
+			a = 0
+			x = (x + 1) % 8
+		glBegin(GL_POLYGON)
+		for vertex in surface:
+			glColor3fv(colors[x])
+			glVertex3fv(vertex)
+		glEnd()
+		a += 1
+
 	glBegin(GL_LINES)
-	#color the for lines you're about to draw
-	glColor3f(1.0, 0.0, 0.0);
+	glColor3f(1, 1, 1)
 	for edge in non.edges:
 		for v in edge:
 			glVertex3fv(v)
@@ -110,12 +197,11 @@ def drag(x,y):
 	glutPostRedisplay()
 
 def keyboard(key,x,y):
-	global non, x_step, z_step, radius_step, height_step
+	global non, x_step, z_step, radius_step, height_step, global_step
 	if key == 27:
 		exit(0)
 	elif key == "x":
 		non.ex += x_step
-		print(non.ex)
 	elif key == "d":
 		non.ex -= x_step
 	elif key == "z":
@@ -130,9 +216,29 @@ def keyboard(key,x,y):
 		non.hc += height_step
 	elif key == "u":
 		non.hc -= height_step
-
+	elif key == "p":
+		printValues()
+	elif key == " ":
+		gradient_descent()
+	elif key == "1":
+		global_step = float(global_step) / 2
+	elif key == "2":
+		global_step = global_step * 2
 	glutPostRedisplay()
 
+def printValues():
+	global non, x_step, z_step, radius_step, height_step, cost, lowest_cost
+	print("x = " + str(non.ex))
+	print("z = " + str(non.tz))
+	print("r = " + str(non.rc))
+	print("h = " + str(non.hc))
+	print("x_step " + str(x_step))
+	print("z_step = " + str(z_step))
+	print("r_step = " + str(radius_step))
+	print("h_step = " + str(height_step))
+	print("global_step = " + str(global_step))
+	print("lowest cost = " + str(lowest_cost))
+	print("current cost = " + str(cost))
 
 def mouse(button,state,x,y):
 	global prevX
